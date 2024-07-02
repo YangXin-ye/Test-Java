c87da7570256ab975a4470b25ef10e58531d7c6d
zhangguohao <15533041512@163.com>
2024-07-02 16:28:19 +0800

    图片上传

--- again/again-web/src/components/pages/product/Product.vue
+++ again/again-web/src/components/pages/product/Product.vue
@@ -53,6 +53,11 @@
     <el-dialog :title="title" :visible.sync="dialogFormVisible" width="30%">
       <el-form label-width="80px" :model="form">
         <el-form-item label="商品图片">
+          <el-upload class="avatar-uploader" action="http://localhost:8080/upload" :headers="header"
+                     :show-file-list="false" :on-success="handleAvatarSuccess" :before-upload="beforeAvatarUpload">
+            <img v-if="imageUrl" :src="imageUrl" class="avatar">
+            <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+          </el-upload>
         </el-form-item>
         <el-form-item label="商品名称">
           <el-input v-model="form.name"></el-input>
@@ -121,6 +126,10 @@ export default {
       inputCategoryOpen: false,
       inputCategory:"",
       inputCategoryNames: new Set(),
+      header:{
+        token:localStorage.getItem('token')
+      },
+      imageUrl:'',
 
     }
   },
@@ -308,7 +317,55 @@ export default {
         this.options = res.data;
 
       })
-    }
+    },
+  //   图片上传
+    handleAvatarSuccess(res, file) {
+      this.form.image = res.data;
+      this.imageUrl = URL.createObjectURL(file.raw);
+    },
+    beforeAvatarUpload(file) {
+      const isJPG = file.type === 'image/jpeg';
+      const isLt2M = file.size / 1024 / 1024 < 2;
+
+      if (!isJPG) {
+        this.$message.error('上传头像图片只能是 JPG 格式!');
+      }
+      if (!isLt2M) {
+        this.$message.error('上传头像图片大小不能超过 2MB!');
+      }
+      return isJPG && isLt2M;
+    },
   }
 }
 </script>
+
+
+<style scoped>
+.avatar-uploader .el-upload {
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
+
+.avatar-uploader .el-upload:hover {
+  border-color: #409EFF;
+}
+
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  line-height: 178px;
+  text-align: center;
+  border: 1px dashed #8c939d;
+}
+
+.avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
+}
+</style>
